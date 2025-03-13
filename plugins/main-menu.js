35e0c590f7acafc1d23e1cc844f0a74d32aeeb97
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-03-13 13:07:56 -0600

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,171 +1,86 @@
-import { promises } from 'fs'
-import { join } from 'path'
-import fetch from 'node-fetch'
-import { xpRange } from '../lib/levelling.js'
+import { thumbnail } from '../exports.js';
+import fetch from 'node-fetch';
 
-let tags = {
-  'main': 'Info 📚',
-  'search': 'Busquedas 🔎',
-  'game': 'Juegos 🎮',
-  'serbot': 'Sub Bots 🤖',
-  'rpg': 'RPG 🌠',
-  'rg': 'Registro 📁',
-  'sticker': 'Stickers 🏞',
-  'img': 'Imágenes 📸',
-  'group': 'Grupos 👥',
-  'logo': 'Logo - maker 🎨',
-  'nable': 'On / Off 📴', 
-  'downloader': 'Descargas 📥',
-  'tools': 'Herramientas 🔧',
-  'fun': 'Diversión 🎲',
-  'nsfw': 'Nsfw 🔞', 
-  'owner': 'Creador 😺', 
-  'audio': 'Audios 🔉', 
-  'advanced': 'Avanzado 💠',
-}
-
-const defaultMenu = {
-  before: `
-*꒷꒦꒷꒷꒦꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒷꒦꒷꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒦꒷*
-
-“ hello *%name*, Cómo se encuentra el día de hoy? ”
-
-╭──⬣「 *Info User* 」⬣
-│  ≡◦ *🍭 Nombre ∙* %name
-│  ≡◦ *🍬 Dulces ∙* %limit
-│  ≡◦ *💫 XP ∙* %totalexp
-│  ≡◦ *🐢 Nivel ∙* %level
-╰──⬣
-%readmore
-*꒷꒦꒷꒷꒦꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒷꒦꒷꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒦꒷*
-
-\t\t\t*L I S T A  -  M E N Ú S*
-`.trimStart(),
-header: '╭──⬣「 *%category* 」⬣',
-body: '│  ≡◦ *%cmd*\n',
-footer: '╰──⬣\n',
-after: '',
-}
-
-let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
-  try {
-    let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
-    let { exp, limit, level } = global.db.data.users[m.sender]
-    let { min, xp, max } = xpRange(level, global.multiplier)
-    let name = await conn.getName(m.sender)
-    let d = new Date(new Date + 3600000)
-    let locale = 'es'
-    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
-    let week = d.toLocaleDateString(locale, { weekday: 'long' })
-    let date = d.toLocaleDateString(locale, {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    })
-    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    }).format(d)
-    let time = d.toLocaleTimeString(locale, {
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
-    })
-    let _uptime = process.uptime() * 1000
-    let _muptime
-    if (process.send) {
-      process.send('uptime')
-      _muptime = await new Promise(resolve => {
-        process.once('message', resolve)
-        setTimeout(resolve, 1000)
-      }) * 1000
-    }
-    let muptime = clockString(_muptime)
-    let uptime = clockString(_uptime)
-    let totalreg = Object.keys(global.db.data.users).length
-    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-    let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
-      return {
-        help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
-        tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
-        prefix: 'customPrefix' in plugin,
-        limit: plugin.limit,
-        premium: plugin.premium,
-        enabled: !plugin.disabled,
-      }
-    })
-    for (let plugin of help)
-      if (plugin && 'tags' in plugin)
-        for (let tag of plugin.tags)
-          if (!(tag in tags) && tag) tags[tag] = tag
-    conn.menu = conn.menu ? conn.menu : {}
-    let before = conn.menu.before || defaultMenu.before
-    let header = conn.menu.header || defaultMenu.header
-    let body = conn.menu.body || defaultMenu.body
-    let footer = conn.menu.footer || defaultMenu.footer
-    let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : ``) + defaultMenu.after
-    let _text = [
-      before,
-      ...Object.keys(tags).map(tag => {
-        return header.replace(/%category/g, tags[tag]) + '\n' + [
-          ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
-            return menu.help.map(help => {
-              return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
-                .replace(/%islimit/g, menu.limit ? '' : '')
-                .replace(/%isPremium/g, menu.premium ? '' : '')
-                .trim()
-            }).join('\n')
-          }),
-          footer
-        ].join('\n')
-      }),
-      after
-    ].join('\n')
-    let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
-    let replace = {
-      '%': '%',
-      p: _p, uptime, muptime,
-      taguser: '@' + m.sender.split("@s.whatsapp.net")[0],
-      wasp: '@0',
-      me: conn.getName(conn.user.jid),
-      npmname: _package.name,
-      version: _package.version,
-      npmdesc: _package.description,
-      npmmain: _package.main,
-      author: _package.author.name,
-      license: _package.license,
-      exp: exp - min,
-      maxexp: xp,
-      totalexp: exp,
-      xp4levelup: max - exp,
-      github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
-      level, limit, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg,
-      readmore: readMore
-    }
-    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+let handler = async (m, { conn }) => {
     
-    let pp = './storage/img/miniurl.jpg'
-    await conn.sendFile(m.chat, pp, 'thumbnail.jpg', text.trim(), m, null)
+    let thumb = await (await fetch(thumbnail)).buffer();
+    let name = await conn.getName(m.sender)
+    let menu = `
+    Hola *${name}* en que puedo ayudarte hoy 😀
 
-  } catch (e) {
-    conn.reply(m.chat, 'Lo sentimos, el menú tiene un error.', m)
-    throw e
-  }
+
+【 𝘔𝘌𝘕𝘜 𝘋𝘌 𝘊𝘖𝘔𝘈𝘕𝘋𝘖𝘚 】
+${readMore}
+
+╭─❮ ${em} *─ INFORMACIÓN ─* ${em} ❯
+├ ▢ *.owner*
+├ ⓘ _Propietario del bot_
+├ ▢ *.ping*
+├ ⓘ _Tiempo de respuesta del servidor_
+├ ▢ *.runtime*
+├ ⓘ _Tiempo encendido_
+├ ▢ *.info*
+├ ⓘ _Información sobre el bot_
+╰─❮ ${em} ❯
+
+╭─❮ ${em} *─ BUSCADORES ─* ${em} ❯
+├ ▢ *.ytsearch* | *.yts*
+├ ⓘ _Buscar videos en YouTube_
+├ ▢ *.spotifys*
+├ ⓘ _Buscar música en Spotify_
+├ ▢ *.pinterest*
+├ ⓘ _Buscar imágenes en Pinterest_
+├ ▢ *.googleimg* | *.goimg*
+├ ⓘ _Buscar imágenes en Google_
+├ ▢ *.tiktoksearch* | *.tts*
+├ ⓘ _Buscar videos en TikTok_
+╰─❮ ${em} ❯
+
+╭─❮ ${em} *─ DESCARGAS ─* ${em} ❯
+├ ▢ *.ytmp4* | *.ytv* | *.ytmp4doc*
+├ ⓘ _Descargar videos de YouTube_
+├ ▢ *.ytmp3* | *.yta* | *.ytmp3doc*
+├ ⓘ _Descargar audios de YouTube_
+├ ▢ *.spotifydl*
+├ ⓘ _Descargar música de Spotify_
+├ ▢ *.tiktok* | *.ttdl*
+├ ⓘ _Descargar videos de TikTok_
+├ ▢ *.facebook* | *.fb*
+├ ⓘ _Descargar videos de Facebook_
+├ ▢ *.instagram* | *.ig*
+├ ⓘ _Descargar videos/fotos de Instagram_
+├ ▢ *.gitclone*
+├ ⓘ _Descargar repositorios de GitHub_
+├ ▢ *.mediafire*
+├ ⓘ _Descargar archivos de Mediafire_
+╰─❮ ${em} ❯
+
+╭─❮ ${em} *─ HERRAMIENTAS ─* ${em} ❯
+├ ▢ *.base64*
+├ ⓘ _Encriptar/Desencriptar textos en base64_
+├ ▢ *.hd*
+├ ⓘ _Mejorar la calidad de imágenes a HD_
+├ ▢ *.morse*
+├ ⓘ _Encriptar/Desencriptar textos en código morse_
+├ ▢ *.toaudio*
+├ ⓘ _Convertidor de video a audio_
+├ ▢ *.upload*
+├ ⓘ _Subir imágenes y obtener enlace_
+├ ▢ *.tts*
+├ ⓘ _Convertidor de texto a voz_
+╰─❮ ${em} ❯
+
+╭─❮ ${em} *─ PROPIETARIO ─* ${em} ❯
+├ ▢ *.enable*
+├ ⓘ _Activar función_
+├ ▢ *.disable*
+├ ⓘ _Desactivar función_
+╰─❮ ${em} ❯
+    `.trim()
+        await conn.sendAiri(m.chat, botname, botdesc, menu, true, thumb, null, m);
 }
-
-handler.help = ['menu']
-handler.tags = ['main']
-handler.command = ['menu', 'help', 'menú'] 
-handler.register = false 
+handler.command = ['menu', 'menú', 'help', 'comandos']
 export default handler
 
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-
-function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
-}
